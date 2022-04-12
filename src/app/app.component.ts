ff877b26a2d8ee5a83b0b15f86c363940b25e1eb
SergeySotnikov <s.sotnikov1990@gmail.com>
2022-04-12 11:52:10 +0300

    1. Added the ability to dynamically change the dashboard background when the button is clicked 2. Added smooth animation when replacing the background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -121,12 +121,71 @@ import { RouterOutlet, UrlSegment } from '@angular/router';
         ]),
       ]),
     ]),
+
+    trigger('bgAnim', [
+      transition(
+        ':leave',
+        animate(
+          1000,
+          style({
+            opacity: 0,
+          })
+        )
+      ),
+    ]),
+
+    trigger('btnAnim', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+        }),
+        animate(
+          250,
+          style({
+            opacity: 1,
+          })
+        ),
+      ]),
+
+      transition(':leave', [
+        animate(
+          250,
+          style({
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
   ],
 })
 export class AppComponent {
+  backgrounds: string[] = ['https://source.unsplash.com/random/1920x1080'];
+  loadingBGImage: boolean = false;
+
   prepareRoute(outlet: RouterOutlet) {
     if (outlet.isActivated) {
       return outlet.activatedRouteData['tab'];
     }
+  }
+
+  async changeBGImage() {
+    this.loadingBGImage = true;
+
+    const result = await fetch('https://source.unsplash.com/random/1920x1080', {
+      method: 'HEAD',
+    });
+
+    const alreadyGot = this.backgrounds.includes(result.url);
+    if (alreadyGot) {
+      this.changeBGImage();
+    }
+    this.backgrounds.push(result.url);
+  }
+
+  onBGImageLoad(imgEvent: Event) {
+    const imgElement = imgEvent.target as HTMLImageElement;
+    const src = imgElement.src;
+    this.backgrounds = this.backgrounds.filter((bg) => bg === src);
+    this.loadingBGImage = false;
   }
 }
